3b3d694966a92db9d9eb8e6eb3828cb7db3df32a
mari-rein <mariecubaka20@gmail.com>
2024-11-22 20:53:29 -0500

    Modif marie

--- forum_frameworks/app/Components/headerpublic.js
+++ forum_frameworks/app/Components/headerpublic.js
@@ -1,6 +1,36 @@
 function Headerpublic() {
     return (
       <nav className="navbar navbar-default navbar-doublerow navbar-trans">
+
+<nav className="navbar navbar-down">
+          <div className="container">
+            <div className="flex-container">
+              <div className="navbar-header flex-item">
+                <a className="navbar-brand" href="#">
+                  <img src="/images/LogoFWF.png" alt="Logo" height={110} width={110} />
+                </a>
+              </div>
+              <ul className="nav navbar-nav flex-item hidden-xs text-center justify-content-center">
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">Forum</a>
+                </li>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+                <li>
+                  <a href="#">FAQ</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
         <nav className="navbar navbar-top">
           <div className="container">
             <ul className="nav navbar-nav pull-left">
@@ -36,7 +66,7 @@ function Headerpublic() {
                 style={{ marginRight: "5px" }}
               />
               <a href="#" className="btn btn-sm btn-success">
-                <span className="glyphicon glyphicon-user"></span> Sign In
+                <span className="glyphicon glyphicon-user"></span> Login
               </a>
               <a href="#" className="btn btn-sm btn-primary" style={{ marginLeft: "5px" }}>
                 <span className="glyphicon glyphicon-certificate"></span> Register
@@ -45,34 +75,7 @@ function Headerpublic() {
           </div>
         </nav>
   
-        <nav className="navbar navbar-down">
-          <div className="container">
-            <div className="flex-container">
-              <div className="navbar-header flex-item">
-                <a className="navbar-brand" href="#">
-                  Geek Connect
-                </a>
-              </div>
-              <ul className="nav navbar-nav flex-item hidden-xs">
-                <li>
-                  <a href="#">Home</a>
-                </li>
-                <li>
-                  <a href="#">About</a>
-                </li>
-                <li>
-                  <a href="#">Forum</a>
-                </li>
-                <li>
-                  <a href="#">Contact</a>
-                </li>
-                <li>
-                  <a href="#">FAQ</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        
       </nav>
     );
   }
